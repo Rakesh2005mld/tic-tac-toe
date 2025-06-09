b7fa66c5de66e609af5106d45fd143b9f1f16e13
Rakesh Sharma <157808930+Rakesh2005mld@users.noreply.github.com>
2025-06-09 20:41:03 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyApkZTMvzGey7iJI-lJAa4xCGXpBIXn0yI",
+    authDomain: "tic-tac-toe-b138a.firebaseapp.com",
+    projectId: "tic-tac-toe-b138a",
+    storageBucket: "tic-tac-toe-b138a.firebasestorage.app",
+    messagingSenderId: "537768597242",
+    appId: "1:537768597242:web:fc741b03745f17d8235b43",
+    measurementId: "G-3FV570MNK9"
+  };
